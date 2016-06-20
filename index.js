6788103c03adbbcde8143d77c820a6bff4fb97f2
Hedy Wang <hedywings@gmail.com>
2016-06-20 14:03:33 +0800

    v0.0.3

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ module.exports = {
             { name: 'nineAxisMeasPeriod', uuid: '0xbb21', params: ['period'], types: ['uint8'] }
         ]
      },
-     analysis: function (periph, basicInfo) {
-        var checkFlag = false;
+     examine: function (periph, basicInfo) {
+        var isMine = false;
 
         if (basicInfo.manufacturer === 'sivann' &&
             basicInfo.devName === 'Nine Axis Sensor' &&
@@ -16,8 +16,8 @@ module.exports = {
             basicInfo.version.fw === 'v1.0.0' && 
             basicInfo.version.hw === 'v1.0.0' &&
             basicInfo.version.sw === 'v1.0.0')
-            checkFlag = true;
+            isMine = true;
 
-        return checkFlag;
+        return isMine;
     }
 };
