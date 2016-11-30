0d4d9f80de7b2ee2eda47346b082d6342f14231e
Eason <onepiece5478@gmail.com>
2016-11-30 18:03:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,15 @@ module.exports = {
             { name: 'aInConfig', uuid: '0xbb11', params: ['config'], types: ['boolean'] },
             { name: 'aInMeasPeriod', uuid: '0xbb12', params: ['period'], types: ['uint8'] },
             { name: 'nineAxisConfig', uuid: '0xbb21', params: ['config'], types: ['boolean'] },
-            { name: 'nineAxisMeasPeriod', uuid: '0xbb22', params: ['period'], types: ['uint8'] }
+            { name: 'nineAxisMeasPeriod', uuid: '0xbb22', params: ['period'], types: ['uint8'] },
+            { name: 'nineAxisResolution', uuid: '0xbb23', params: ['resolution'], types: ['uint8'] }
         ]
      },
      examine: function (periph, basicInfo) {
         var isMine = false;
 
         if (basicInfo.manufacturer === 'sivann' &&
-            basicInfo.devName === 'Nine Axis Sensor' &&
-            basicInfo.model === 'NineAxisSensor' &&
-            basicInfo.fwRev === 'v1.0.0' && 
-            basicInfo.hwRev === 'v1.0.0' &&
-            basicInfo.swRev === 'v1.0.0')
+            basicInfo.devName === 'Nine Axis Sensor')
             isMine = true;
 
         return isMine;
